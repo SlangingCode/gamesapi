78f27c4a9aed47a5f824b9ef498c65e0e40e0342
Geoff Cox <slangingcode@gmail.com>
2016-12-27 10:11:30 -0500

    Create Players, Game Categories, and Scores using generalized class behaviors

--- games/migrations/0001_initial.py
+++ games/migrations/0001_initial.py
@@ -13,15 +13,54 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Game',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('name', models.CharField(default='', max_length=200, blank=True)),
                 ('release_date', models.DateTimeField()),
-                ('game_category', models.CharField(default='', max_length=200, blank=True)),
                 ('played', models.BooleanField(default=False)),
             ],
             options={
                 'ordering': ('name',),
             },
+        ),
+        migrations.CreateModel(
+            name='GameCategory',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=200)),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Player',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('name', models.CharField(default='', max_length=50)),
+                ('gender', models.CharField(default='M', max_length=2, choices=[('M', 'Male'), ('F', 'Female')])),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+        migrations.CreateModel(
+            name='PlayerScore',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('score', models.IntegerField()),
+                ('score_date', models.DateTimeField()),
+                ('game', models.ForeignKey(to='games.Game')),
+                ('player', models.ForeignKey(to='games.Player', related_name='scores')),
+            ],
+            options={
+                'ordering': ('-score',),
+            },
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='game_category',
+            field=models.ForeignKey(to='games.GameCategory', related_name='games'),
         ),
     ]
